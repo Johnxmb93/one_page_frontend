37a6d6c2bb955033aa4dba12a80af9d6ed1df7b8
Jonathan Bahena <jonathan.mb93@gmail.com>
2021-09-13 19:55:50 -0500

    inital commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-for="product in products" v-bind:key="product.id">
+      Name:
+      <h1>{{ product.name }}</h1>
+      Description:
+      <h1>{{ product.description }}</h1>
+      Price:
+      <h1>${{ product.price }}</h1>
+      <img v-bind:src="product.image_url" :alt="product.name" contain height="300px" width="300px" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("/products").then((response) => {
+        this.products = response.data;
+        console.log(this.product_id);
+      });
+    },
   },
 };
 </script>
